4ade6b4c5f2807c55a1941f9c11df9fae4c90fc1
JCamiloo <jcamilo.osorio15@gmail.com>
2020-10-05 17:20:41 -0500

    using screen orientation and platform api

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Platform, StyleSheet } from 'react-native';
 import TitleText from './TitleText';
 import Colors from '../constants/colors';
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.header,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid
+        })
+      }}
+    >
       <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
     alignItems: 'center',
     justifyContent: 'center'
   },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: 'transparent',
+    borderBottomWidth: 0
+  },
   headerTitle: {
-    color: 'black',
-    fontSize: 18
+    color: Platform.OS === 'ios' ? Colors.primary : 'white'
   }
 });
 
